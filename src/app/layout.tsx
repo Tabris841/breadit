4651cc5d7d22b1e373344c72a81f0e8c863fd67e
Anton Todorov <shadowtabris84@gmail.com>
2023-07-15 17:55:56 +0300

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,44 @@
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
 
 export const metadata: Metadata = {
-  title: 'Reddit Clone',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: "Reddit Clone",
+  description: "A Reddit clone built with Next.js and TypeScript.",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "light bg-white  text-slate-900 antialiased",
+        inter.className,
+      )}
+    >
+      <body className="min-h-screen bg-slate-50 pt-12 antialiased">
+        <Navbar />
+
+        {authModal}
+
+        <div className="container mx-auto h-full max-w-7xl pt-12">
+          {children}
+        </div>
+
+        <Toaster />
+      </body>
     </html>
   );
 }
